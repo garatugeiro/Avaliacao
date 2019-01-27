43d8c23c2292df675de85d37af590cad426308cd
Filipe Garatugeiro <46558651+garatugeiro@users.noreply.github.com>
2019-01-27 22:16:51 +0000

    Add files via upload

---
+++ avaliacao/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo ?></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="h-left">
+            <h1><?php echo $t_header ?></h1>
+        </div>
+        <div class="h-right">
+            <ul id="butoes">
+                <a href="index.php">HTML</a>
+                <a href="css.php">CSS</a>
+                <a href="js.php">JS</a>
+                <a href="php.php">PHP</a>
+                <a href="mysql.php">MYSQL</a>
+            </ul>
+        </div>
+    </header>
